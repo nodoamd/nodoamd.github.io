dff6b5ad183049ec48e417ef53a621f8fb1760aa
nodoamd <nodoagenciadigital@hotmail.com>
2025-05-09 13:40:10 +0200

    clamp

---
+++ clamp/script.js
@@ -0,0 +1,25 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, DrawSVGPlugin);
+
+// Crear el efecto de scroll suave
+let smoother = ScrollSmoother.create({
+  smooth: 2,
+  effects: true
+});
+
+// Animación de trazado SVG
+if (document.querySelector('.draw') && document.querySelector('.heading')) {
+  gsap.from('.draw', {
+    drawSVG: "0%",
+    ease: "expo.out",
+    scrollTrigger: {
+      trigger: '.heading',
+      start: "clamp(top center)",
+      scrub: true,
+      pin: '.pin',
+      pinSpacing: false,
+    }
+  });
+}
+
+// Setup visual
+gsap.set(".logo svg", { opacity: 1 });
