815edb312081de4f0648f73dd78501e87d4ba687
nodoamd <nodoagenciadigital@hotmail.com>
2025-06-14 17:15:15 +0200

    ssdc

--- tests/script.js
+++ tests/script.js
@@ -46,17 +46,3 @@ gsap.from(".text-card", {
     stagger: 0.3,
     ease: "power3.out"
 });
-
-// Animación para los textos dentro de las tarjetas
-gsap.from([".overline", ".about-title, .exp-title", ".about-desc, .exp-desc", ".highlight-text"], {
-    scrollTrigger: {
-        trigger: ".text-card",
-        start: "top bottom-=100",
-        toggleActions: "play none none reverse"
-    },
-    y: 20,
-    opacity: 0,
-    duration: 0.8,
-    stagger: 0.2,
-    ease: "power2.out"
-});
