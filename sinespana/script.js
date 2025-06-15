8972fda8b0f9cb41fcdc6c4c0456fdb28fc838d4
nodoamd <nodoagenciadigital@hotmail.com>
2025-06-15 20:08:45 +0200

    abbcv

--- sinespana/script.js
+++ sinespana/script.js
@@ -5,7 +5,7 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 if (isMobile) {
     // Configuración de los puntos de anclaje
-    const sections = [".hero", ".heading", ".about-hero"];
+    const sections = [".hero", ".heading"];
     
     // Crear un ScrollTrigger principal que maneje todos los snaps
     ScrollTrigger.create({
