3ce996a7a50f7f6292c22810b33cccda16adb940
nodoamd <nodoagenciadigital@hotmail.com>
2025-07-15 15:53:50 +0200

    ddc

--- tests/script.js
+++ tests/script.js
@@ -80,33 +80,33 @@ ScrollTrigger.create({
 // Setup visual
 gsap.set(".logo svg", { opacity: 1 });
 
-// Animación para las tarjetas de texto
-gsap.from(".text-card", {
-    scrollTrigger: {
-        trigger: ".text-card",
-        start: "top bottom-=100",
-        toggleActions: "play none none reverse"
-    },
-    y: 50,
-    opacity: 0,
-    duration: 1,
-    stagger: 0.3,
-    ease: "power3.out"
-});
+// // Animación para las tarjetas de texto
+// gsap.from(".text-card", {
+//     scrollTrigger: {
+//         trigger: ".text-card",
+//         start: "top bottom-=100",
+//         toggleActions: "play none none reverse"
+//     },
+//     y: 50,
+//     opacity: 0,
+//     duration: 1,
+//     stagger: 0.3,
+//     ease: "power3.out"
+// });
 
-// Animación para los textos dentro de las tarjetas
-gsap.from([".overline", ".about-title, .exp-title", ".about-desc, .exp-desc", ".highlight-text"], {
-    scrollTrigger: {
-        trigger: ".text-card",
-        start: "top bottom-=100",
-        toggleActions: "play none none reverse"
-    },
-    y: 20,
-    opacity: 0,
-    duration: 0.8,
-    stagger: 0.2,
-    ease: "power2.out"
-});
+// // Animación para los textos dentro de las tarjetas
+// gsap.from([".overline", ".about-title, .exp-title", ".about-desc, .exp-desc", ".highlight-text"], {
+//     scrollTrigger: {
+//         trigger: ".text-card",
+//         start: "top bottom-=100",
+//         toggleActions: "play none none reverse"
+//     },
+//     y: 20,
+//     opacity: 0,
+//     duration: 0.8,
+//     stagger: 0.2,
+//     ease: "power2.out"
+// });
 
 
 document.addEventListener('DOMContentLoaded', function () {
